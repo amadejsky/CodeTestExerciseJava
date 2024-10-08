2af52927af5916ad7a19fd4553944cee6e792c6c
amadejsky <adrian.madejski@vp.pl>
2024-10-08 09:00:06 +0200

    Usprawnienie metody szczegóły(), oraz metody rosnij(), dodatkowe pole nazwa w klasie abstrakcyjnej i modelach, dwa przykłady drzew oraz wywołania metod szczegoly() i rosnij() na obu Encjach

---
+++ rest-manager-task02/src/main/java/com/amadejsky/rest_manager_task02/RestManagerTask02Application.java
@@ -0,0 +1,13 @@
+package com.amadejsky.rest_manager_task02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestManagerTask02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestManagerTask02Application.class, args);
+	}
+
+}
---
+++ rest-manager-task02/src/test/java/com/amadejsky/rest_manager_task02/RestManagerTask02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amadejsky.rest_manager_task02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestManagerTask02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
